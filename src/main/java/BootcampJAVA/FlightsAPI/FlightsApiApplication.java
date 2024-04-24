014e81396aca9c7a4ea188a053c6b4dc3af8ac97
Marcela Wojtasik <marcelawojtasik@gmail.com>
2024-04-24 09:58:31 -0300

    Eureka client

--- src/main/java/BootcampJAVA/FlightsAPI/FlightsApiApplication.java
+++ src/main/java/BootcampJAVA/FlightsAPI/FlightsApiApplication.java
@@ -2,8 +2,10 @@ package BootcampJAVA.FlightsAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FlightsApiApplication {
 
 	public static void main(String[] args) {
